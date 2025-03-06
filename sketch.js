c7b9113a6a07036a06f15db8de02059f6d5fafe9
Grace <gracek2705@tuckahoeschools.org>
2025-03-06 13:31:31 +0000

    commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  function draw() {
+    background(220);
+  }
+  function draw() {
+    //sky blue background
+    background(135, 206, 235);
+    circle(550, 50, 100);
+    fill("yellow");
+  }
